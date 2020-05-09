b5ebed722a228215f855aa1756cfbccffc47437a
fissha <superfissha@hotmail.com>
2020-05-09 07:34:24 +0200

    Subir todos los achivos pulsando el doble ++ para tener el proyecto base en el servidor remoto.

---
+++ BaseProject/src/main/java/com/jpinformatica/MavenStandard/BaseProject/App.java
@@ -0,0 +1,20 @@
+package com.jpinformatica.MavenStandard.BaseProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	static int uno;
+	static int dos;
+	
+    public static void main( String[] args )
+    {
+    	uno = 1;
+    	dos = 2;
+    	System.out.println( "Hello World!" );
+    	System.out.println( uno + dos );
+    	System.out.println( "Hello World!" + (uno + dos) );
+    }
+}
